667ceb5ca228ebc1843f61ea0f0e12f75171518d
Odden69 <helena.joh2@gmail.com>
2022-06-14 19:12:14 +0000

    complete webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
-class OrderLineItemAdminInline(admin.TabularInline):  # Presents a list of editable order rows instead of having to 
-    # go in to a specific order row to edit 
+class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
@@ -11,21 +10,22 @@ class OrderLineItemAdminInline(admin.TabularInline):  # Presents a list of edita
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date',  # prevents editing of the values in these fields
+    readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name', # order of fields in the form
+    fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
-    list_display = ('order_number', 'date', 'full_name', # Column display in the order list
+    list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
                     'grand_total',)
 
-    ordering = ('-date',)  # Showing the order list with the most recent order at the top
-
+    ordering = ('-date',)
 
 admin.site.register(Order, OrderAdmin)
