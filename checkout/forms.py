43a274dfabb6d576a5537ce11c1c03ffc15da1ff
Odden69 <helena.joh2@gmail.com>
2022-06-15 13:05:37 +0000

    update checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -30,7 +30,7 @@ class OrderForm(forms.ModelForm):
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'country':
-                if self.fields[field].required:
+                if self.fields[field].required:                  
                     placeholder = f'{placeholders[field]} *'
                 else:
                     placeholder = placeholders[field]
